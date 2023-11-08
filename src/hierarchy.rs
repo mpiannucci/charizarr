ba9c8c6f7f83618c61ae9384d1db029049db42b0
Matthew Iannucci <mpiannucci@gmail.com>
2023-11-08 15:34:02 -0500

    Start building hierarchies for arrays and groups

--- src/hierarchy.rs
+++ src/hierarchy.rs
@@ -1,17 +1,48 @@
-use crate::store::{ListableStore, ReadableStore, WriteableStore};
+use crate::{
+    metadata::{ArrayMetadata, GroupMetadata},
+    store::{ListableStore, ReadableStore, WriteableStore},
+};
 
-pub struct Hierarchy<'a, T>
+pub struct Array<'a, T>
 where
     T: ReadableStore + ListableStore + WriteableStore,
 {
     store: &'a T,
+    metadata: ArrayMetadata,
+    root_path: String,
 }
 
-impl<'a, T> Hierarchy<'a, T>
+impl<'a, T> Array<'a, T>
 where
     T: ReadableStore + ListableStore + WriteableStore,
 {
-    pub fn new(store: &'a T) -> Self {
-        Self { store }
+    pub fn new(store: &'a T, metadata: ArrayMetadata, root_path: Option<String>) -> Self {
+        Self {
+            store,
+            metadata,
+            root_path: root_path.unwrap_or_else(|| "".to_string()),
+        }
+    }
+}
+
+pub struct Group<'a, T>
+where
+    T: ReadableStore + ListableStore + WriteableStore,
+{
+    store: &'a T,
+    // metadata: GroupMetadata,
+    root_path: String,
+}
+
+impl<'a, T> Group<'a, T>
+where
+    T: ReadableStore + ListableStore + WriteableStore,
+{
+    pub async fn open(store: &'a T, root_path: Option<String>) -> Result<Self, String> {
+        Ok(Self {
+            store,
+            // metadata,
+            root_path: root_path.unwrap_or_else(|| "".to_string()),
+        })
     }
 }
